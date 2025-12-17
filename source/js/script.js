93badae4677141af310f7121a3ac81e6466c7492
Sopha-ling <71763257+Sopha-ling@users.noreply.github.com>
2025-12-17 13:02:22 +0300

    лишнее

--- source/js/script.js
+++
@@ -1,8 +0,0 @@
-if (document.querySelector('.no-js')) {
-  document.querySelector('.no-js').classList.remove('no-js');
-}
-
-const menu = document.querySelector('.menu');
-const menuToggle = document.querySelector('.menu__toggle');
-
-menuToggle.addEventListener('click', () => menu.classList.toggle('menu--opened'));
